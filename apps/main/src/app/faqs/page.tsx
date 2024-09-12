d8819422b1e9061f2e2cfff56fcbf350ee0eda37
shobhitexe <sguptamds94@gmail.com>
2024-09-12 19:57:03 +0530

    Updates

--- apps/main/src/app/faqs/page.tsx
+++ apps/main/src/app/faqs/page.tsx
@@ -32,9 +32,9 @@ const FaqsArray = [
 
 export default function page() {
   return (
-    <div className="mt-28 flex sm:flex-row flex-col justify-center sm:gap-10 gap-5 sm:px-10 px-5">
+    <div className="mt-28 flex sm:flex-row flex-col justify-around md:gap-10 gap-5 sm:px-10 px-5">
       <div>
-        <GoldenText className="font-Kugile sm:text-5xl xs:text-4xl text-3xl font-semibold py-2">
+        <GoldenText className="font-Kugile md:text-5xl xs:text-4xl text-3xl font-semibold py-2">
           Get all your <br /> questions answered{" "}
         </GoldenText>
         <div className="sm:text-lg text-base font-ClashGroteskLight">
@@ -49,7 +49,7 @@ export default function page() {
       <Accordion type="single" collapsible className="sm:max-w-md w-full">
         {FaqsArray.map((item, index) => (
           <AccordionItem value={`item-${index + 1}`} className="mt-5 relative">
-            <div className="absolute inset-x-0 h-px sm:w-full w-1/4 mx-auto -top-px shadow-2xl bg-gradient-to-r from-transparent via-white/60 to-transparent" />
+            <div className="absolute inset-x-0 h-px sm:w-full w-1/2 mx-auto -top-px shadow-2xl bg-gradient-to-r from-transparent via-white/60 to-transparent" />
 
             <AccordionTrigger className="text-Apricot font-medium">
               {item.que}
