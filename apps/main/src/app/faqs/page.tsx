8d8526f2b7ed4a3a03190bfc4b9f088ff385e170
Shobhit <sguptamds94@gmail.com>
2024-11-06 16:14:39 +0530

    Fixes

--- apps/main/src/app/faqs/page.tsx
+++ apps/main/src/app/faqs/page.tsx
@@ -8,6 +8,11 @@ import {
 } from "@repo/ui";
 import { FaqsArray } from "./FaqsArray";
 
+const Headings: Record<number, string> = {
+  7: "General Questions",
+  14: "Payouts",
+};
+
 export default function page() {
   return (
     <div className="mt-28 flex flex-col justify-around md:gap-10 gap-5 sm:px-10 px-5 sm:w-[90%] mx-auto">
@@ -26,21 +31,27 @@ export default function page() {
       </div>
       <Accordion type="single" collapsible className="sm:max-w-mds w-full">
         {FaqsArray.map((item, index) => (
-          <AccordionItem value={`item-${index + 1}`} className="mt-5 relative">
-            <div className="absolute inset-x-0 h-px sm:w-full w-1/2 mx-auto -top-px shadow-2xl bg-gradient-to-r from-transparent via-white/60 to-transparent" />
+          <>
+            <div className="text-2xl mt-5">{Headings[index]}</div>
+            <AccordionItem
+              value={`item-${index + 1}`}
+              className="mt-5 relative"
+            >
+              <div className="absolute inset-x-0 h-px sm:w-full w-1/2 mx-auto -top-px shadow-2xl bg-gradient-to-r from-transparent via-white/60 to-transparent" />
 
-            <AccordionTrigger className="text-Apricot font-medium">
-              {item.que}
-            </AccordionTrigger>
-            <AccordionContent className="text-SilverFoil text-base">
-              {item.ans.split("<br/>").map((text) => (
-                <div key={text}>
-                  {text}
-                  <br />
-                </div>
-              ))}
-            </AccordionContent>
-          </AccordionItem>
+              <AccordionTrigger className="text-Apricot font-medium">
+                {item.que}
+              </AccordionTrigger>
+              <AccordionContent className="text-SilverFoil text-base">
+                {item.ans.split("<br/>").map((text) => (
+                  <div key={text}>
+                    {text}
+                    <br />
+                  </div>
+                ))}
+              </AccordionContent>
+            </AccordionItem>
+          </>
         ))}
       </Accordion>
     </div>
